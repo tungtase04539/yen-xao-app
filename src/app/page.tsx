584598f09d7194d7d4ed65d0f1c330362db614bb
TÃ¹ng <legendarytvst@gmail.com>
2026-03-08 08:03:48 +0700

    remove: WhyChooseUs section from homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import HeroSlider from '@/components/home/HeroSlider';
 import FeaturedProducts from '@/components/home/FeaturedProducts';
 import CategoryTabs from '@/components/home/CategoryTabs';
-import WhyChooseUs from '@/components/home/WhyChooseUs';
+
 import BrandStory from '@/components/home/BrandStory';
 import Certifications from '@/components/home/Certifications';
 import FlashSale from '@/components/home/FlashSale';
@@ -14,7 +14,7 @@ export default function Home() {
     <>
       <HeroSlider />
       <ScrollReveal><FeaturedProducts /></ScrollReveal>
-      <ScrollReveal><WhyChooseUs /></ScrollReveal>
+
       <ScrollReveal><CategoryTabs /></ScrollReveal>
       <ScrollReveal><BrandStory /></ScrollReveal>
       <ScrollReveal><FlashSale /></ScrollReveal>
