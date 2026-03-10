f889a68c3bb7a37f4d7d0fb940828b0735021f25
TÃ¹ng <legendarytvst@gmail.com>
2026-03-10 22:29:07 +0700

    fix: move CTA buttons up to bottom-44

--- src/components/home/HeroSlider.tsx
+++ src/components/home/HeroSlider.tsx
@@ -198,7 +198,7 @@ export default function HeroSlider() {
                   initial={{ y: 40, opacity: 0 }}
                   animate={{ y: 0, opacity: 1 }}
                   transition={{ delay: slide.title ? 0.65 : 0.3, duration: 0.6 }}
-                  className="absolute bottom-36 left-4 md:left-8 flex flex-wrap gap-4"
+                  className="absolute bottom-44 left-4 md:left-8 flex flex-wrap gap-4"
                 >
                   <Link
                     href={slide.button_link}
